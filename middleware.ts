e0d5c50832b8f7ae24320260bbac92eecc9674bc
crazywoola <li.zheng@dentsplysirona.com>
2023-04-17 10:36:56 +0800

    fix: token verified

--- middleware.ts
+++ middleware.ts
@@ -10,6 +10,7 @@ export const middleware = async (request: NextRequest) => {
   const response = NextResponse.next()
   try {
     const token = request.nextUrl.searchParams.get('access_token')
+
     if (token) {
       console.log(`JWT Token: ${token}`)
       await jose.jwtVerify(token, secret, {
@@ -17,8 +18,10 @@ export const middleware = async (request: NextRequest) => {
         subject: 'LangGenius:CE:Auth',
       })
       response.cookies.set('access_token', token)
+      console.log('JWT Token verified')
     }
     else {
+      console.log('JWT Token not verified')
       return new Error('No token provided')
     }
   }
